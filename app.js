f1e01fbb0374015ed6654434fa316951f3b9f53d
druloloy <druloloy@proton.me>
2023-07-01 00:34:05 +0800

    removed app.js and test file

--- app.js
+++
@@ -1,8 +0,0 @@
-const excel2json = require('./lib/convert-excel-to-json');
-
-const result = excel2json({
-    sourceFile: './tests/sample.xlsx',
-    includeEmptyCells: true
-});
-
-console.log(result)
